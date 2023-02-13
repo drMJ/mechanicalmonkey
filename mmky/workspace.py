51475db5fb71081c117ddefba6d824b69327b508
drMJ <drMJ@users.noreply.github.com>
2023-02-13 14:38:09 -0800

    workspace limits

--- mmky/workspace.py
+++ mmky/workspace.py
@@ -5,10 +5,11 @@ from roman import Robot, Tool, Joints, GraspMode
 
 
 class Workspace:
-    def __init__(self, radius_range, angular_range, height=0, start_position=None, out_position=None, neutral_position=None):
+    def __init__(self, radius_range, angular_range, height_range, start_position=None, out_position=None, neutral_position=None):
         self.radius_range = radius_range
         self.angular_range = angular_range
-        self.height = height
+        self.height = height_range[0]
+        self.height_range = height_range
         self.start = eval(start_position) if isinstance(start_position, str) else start_position
         self.out = eval(out_position) if isinstance(out_position, str) else out_position
         self.neutral = eval(neutral_position) if isinstance(neutral_position, str) else neutral_position
@@ -49,5 +50,15 @@ class Workspace:
         # Sample a random distance from the coordinate origin (i.e., arm base) and a random angle.
         dist = self.radius_range[0] + random.random() * (self.radius_range[1] - self.radius_range[0])
         angle = self.angular_range[0] + random.random() * (self.angular_range[1] - self.angular_range[0])
-        return [dist * math.cos(angle), dist * math.sin(angle)]
+        return [-dist * math.cos(angle), dist * math.sin(angle)]
+
+    def in_range(value, range):
+        return value >= range[0] and value <= range[1]
+
+    def is_inside(self, arm_state):
+        return  in_range(arm.tool_pose()[Tool.Z], self.height_range) and
+                in_range(np.linalg.norm(arm.tool_pose()[:2]), self.radius_range) and
+                in_range(arm.joint_positions()[Joints.Base], self.angular_range)
+
+
 
